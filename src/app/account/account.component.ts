06c72fc17cc1db9f788294df1e2838b96d09dc5b
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-10-16 22:03:13 -0400

    Final version

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -12,12 +12,12 @@ export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
  
-  constructor(private loggingService: LoggingService,
-    private accountsService: AccountsService) {
+  constructor(private accountsService: AccountsService) {
   }
 
   onSetTo(status: string) {
     this.accountsService.updateStatus(this.id, status);
     // this.loggingService.logStatusChange(status);
+    this.accountsService.statusUpdated.emit(status);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -12,6 +12,9 @@ export class NewAccountComponent {
 
   constructor(private loggingService: LoggingService,
     private accountsService: AccountsService) {
+      this.accountsService.statusUpdated.subscribe(
+        (status: string) => alert(`New Status: ${status}`)
+      );
   }
 
   onCreateAccount(accountName: string, accountStatus: string) {
